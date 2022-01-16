740f734f4f4d880ca643abc8517da5e0531740cc
Aleksei Petrov <alexey.webmail@gmail.com>
2022-01-16 15:15:02 +0300

    Play game function

--- SeaBattle/src/Main.java
+++ SeaBattle/src/Main.java
@@ -24,5 +24,89 @@ public class Main
     }
 
     private static void fillPlayerField(char[][] playerField)
-    {}
+    {
+        // i - number of ship desks
+        for (int i = 4; i >= 1; i--)
+        {
+            int ships = 5 - i;
+
+            for (int k = i; k <= ships; k++)
+            {
+                System.out.println("Расставляем " + i + "-палубный корбль. Осталось расставить: " + (k + 1));
+
+                System.out.println("Input x coord: ");
+                int x = scanner.nextInt();
+
+                System.out.println("Input y coord: ");
+                int y = scanner.nextInt();
+                
+                System.out.println("1 - horizontal; 2 - vertical ?");
+                int position = scanner.nextInt();
+                
+                if (position == 1) {
+                    
+                    for (int q = 0; q < i; q++) {
+                        playerField[y][x + q] = '1';
+                    }
+                } else {
+                    
+                    for (int m = 0; m < i; m++) {
+                        playerField[y + m][x] = '1';
+                    }
+                }
+                
+                printField(playerField);
+            }
+        }
+    }
+    
+    private static void printField(char[][] field) {
+        
+        for (char[] cells : field) {
+            
+            for (char cell : cells) {
+                
+                if (cell == 0) {
+                    System.out.print(" |");
+                } else {
+                    System.out.print(cell + "|");
+                }
+            }
+        }
+        
+        System.out.println("");
+        System.out.println("--------------------");
+    }
+    
+    private static void playGame(String player1Name, String player2Name, char[][] playerField1, char[][] playerField2) {
+        char[][] playerBattleField1 = new char[FIELD_LENGTH][FIELD_LENGTH];
+        char[][] playerBattleField2 = new char[FIELD_LENGTH][FIELD_LENGTH];
+        
+        String currentPlayerName = player1Name;
+        char[][] currentPlayerField = playerField2;
+        char[][] currentPlayerBattleField = playerBattleField1;
+        
+        while (isPlayerAlive(playerField1) && isPlayerAlive(playerField2)) {
+            System.out.println(currentPlayerName + ", please, input x coord of shot");
+            int xShot = scanner.nextInt();
+            
+            System.out.println(currentPlayerName + ", please, input y coord of shot");
+            int yShot = scanner.nextInt();
+            
+            int shotResult = handleShot(currentPlayerBattleField, currentPlayerField, xShot, yShot);
+            
+            if (shotResult == 0) {
+                currentPlayerName = player2Name;
+                currentPlayerField = playerField1;
+                currentPlayerBattleField = playerBattleField2;
+            }
+        }
+    }
+    
+    private static int handleShot(char[][] battleFirld, char[][] field, int x, int y) {
+        
+        if ("1".equals(field[y][x])) {
+            field[y][x] = '#';
+        }
+    }
 }
